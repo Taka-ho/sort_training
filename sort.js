723a0370822c57b2ed2313ab56b796238bb5c16b
Taka-ho1 <j1403239@gmail.com>
2021-08-09 17:28:51 +0900

    pointの番号生成のfor文を作り直した

--- sort.js
+++ sort.js
@@ -11,37 +11,46 @@ showLog(arrayToSort, 'ソート前'); // index.htmlへ表示
 // 自分でarrayToSortの中身を操作して、並べ替えの処理を記述する。
 // ---- start 
 const length = arrayToSort.length
-function makeSort(pointNumber,comparedNumber) {
-    makePoint();
+
+function makePoint(){
     
+    //arrayToSort-1で一番最後の配列を指すので、その数字をループ文で増やしていく
+    for (let pointNumber = 1; pointNumber < length; pointNumber++){
+        console.log(pointNumber);
+    }
+    
+    for (let comparedNumber = 2; comparedNumber < length-1; comparedNumber++){
+        console.log(comparedNumber);
+    }
+   
+}
+
+function makeSort(pointNumber,comparedNumber) {
     arrayToSort = [];
     //配列の最後と最後から二番目を比べて入れ替えるかどうかの処理を書く
-    for (sortTimes = 0; sortTimes = length*length; sortTimes++){
-        if (arrayToSort-comparedNumber < arrayToSort-pointNumber){
+    for (sortTimes = 0; sortTimes < length*length; sortTimes++)
+        if (arrayToSort- + comparedNumber < arrayToSort- + pointNumber){
+            let tmpVal = arrayToSort[arrayToSort- + comparedNumber]
+            arrayToSort[arrayToSort- + comparedNumber] = arrayToSort- + PointNumber;
+            arrayToSort[arrayToSort- + comparedNumber] = tmpVal
+            arrayToSort.push(arrayToSort[arrayToSort- + comparedNumber] = tmpVal);
 
-        
     //小さかった場合は入れ替えていく
     }else if(arrayToSort-pointNumber < arrayToSort-comparedNumber){
+         let tmpVal = arrayToSort[arrayToSort- + pointNumber]
+         arrayToSort[arrayToSort- + pointNumber] = arrayToSort- + comparedNumber;
+         arrayToSort[arrayToSort- + comparedNumber] = tmpVal
+         arrayToSort.push(arrayToSort[arrayToSort[arrayToSort- + comparedNumber] = tmpVal] = tmpVal);
 
-    }
-    arrayToSort.push(/*この中にソートした配列を入れる*/);
+        }else if(arrayToSort- + pointNumber === arrayToSort- + comparedNumber){
 
+        }
+
+        return arrayToSort;
     }
-    return arrayToSort;
-}
+
+
 
 showLog(arrayToSort, 'ソート後'); // 結果を表示
 
-function makePoint(){
-    //arrayToSort-1で一番最後の配列を指すので、その数字をループ文で増やしていく
-    var pointNumber = 1;
-    for (pointNumber = 1; pointNumber = length; pointNumber++){
-        
-    }
-    
-    var comparedNumber = 2;
-    for (comparedNumber = 2; comparedNumber = length-1; comparedNumber++){
-    
-    }
-    return pointNumber,comparedNumber;    
-}
+
